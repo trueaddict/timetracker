76066683f6e36d52bd027607d0962763ed48720c
trueaddict <50746660+trueaddict@users.noreply.github.com>
2020-04-01 20:10:02 +0300

    Tallentaa kokeilu datan json-fileen

---
+++ timetracker.py
@@ -0,0 +1,43 @@
+import json
+import time
+from datetime import date
+
+
+def timetracker():
+    readJson()
+    initData()
+
+
+def initData():
+    with open('data/week14.json', 'r') as r_file:
+        data = json.load(r_file)
+        day =   {   "date" : "",
+                    "uni" : 0,
+                    "opiskelu" : 0,
+                    "tyot" : 0,
+                    "muu" : 0
+                }
+        day["date"] = str(date.today())
+        data.append(day)
+
+        print(data)
+        r_file.close()
+        saveData(data)
+        
+
+def saveData(data):
+    with open('data/week14.json', 'w') as w_file:
+        json.dump(data, w_file)
+
+
+def readJson():
+    weeknumber = getWeek()
+    filePath = '/data/week' + str(weeknumber) +'.json'
+
+
+def getWeek():
+    return date.today().isocalendar()[1]
+
+
+if __name__ == '__main__':
+    timetracker()
